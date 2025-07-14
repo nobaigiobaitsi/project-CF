f80d0212a6a1974395648beb2c7909e3678e98e4
Stavros Angelopoulos <steveangelopoulos@gmail.com>
2025-07-14 21:52:12 +0300

    Added a frontend and connected the db. Also styled it with ready made files given by the professor.

---
+++ src/main/java/gr/aueb/cf/schoolapp/controller/TeacherController.java
@@ -0,0 +1,29 @@
+package gr.aueb.cf.schoolapp.controller;
+
+import gr.aueb.cf.schoolapp.dto.TeacherInsertDTO;
+import gr.aueb.cf.schoolapp.repository.RegionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/school/teachers")
+public class TeacherController {
+
+    private final RegionRepository regionRepository;
+
+    @Autowired
+    public TeacherController(RegionRepository regionRepository) {
+        this.regionRepository = regionRepository;
+    }
+
+    @GetMapping("/insert")
+    public String getTeacherForm(Model model) {
+        model.addAttribute("teacherInsertDTO", new TeacherInsertDTO());
+        model.addAttribute("regions", regionRepository.findAll());
+        return "teacher-form";
+    }
+
+}
